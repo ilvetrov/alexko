c3224866890b48181b0a7679006bd118226cbf25
ilvetrov <ilya-ilvetrov@gmail.com>
2021-05-24 20:29:28 +0800

    new cache

--- libs/cache.js
+++ libs/cache.js
@@ -1,31 +1,56 @@
 let cacheStorage = {};
 
+const defaultMsToClear = 1000 * 60 * 60 * 24;
+
 function get(cacheName) {
   return cacheStorage[cacheName];
 }
 
-function set(cacheName, value) {
+function set(cacheName, value, msToClear = defaultMsToClear) {
   cacheStorage[cacheName] = value;
+
+  setTimeout(() => {
+    if (cacheStorage[cacheName]) delete cacheStorage[cacheName];
+  }, msToClear);
 }
 
 function remove(cacheName) {
   delete cacheStorage[cacheName];
 }
 
-function getOrSet(cacheName, getValueFunction) {
+function getOrSet(cacheName, getValueFunction, msToClear = defaultMsToClear) {
   const cached = get(cacheName);
   if (cached) {
-    return cached;
+
+    if (getValueFunction.constructor.name === 'AsyncFunction') {
+      return new Promise(function(resolve, reject) {
+        resolve(cached);
+      });
+    } else {
+      return cached;
+    }
+    
   } else {
-    const newValue = getValueFunction();
-    set(cacheName, newValue);
-    return newValue;
+
+    if (getValueFunction.constructor.name === 'AsyncFunction') {
+      return new Promise(function(resolve, reject) {
+        getValueFunction().then(function(newValue) {
+          set(cacheName, newValue, msToClear);
+          resolve(newValue);
+        });
+      });
+    } else {
+      const newValue = getValueFunction();
+      set(cacheName, newValue, msToClear);
+      return newValue;
+    }
+
   }
 }
 
 setInterval(() => {
   cacheStorage = {};
-}, 1000 * 60 * 60 * 24);
+}, defaultMsToClear);
 
 const cache = {
   getOrSet,
