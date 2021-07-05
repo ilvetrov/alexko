b8615e38ee82922d252f77fb9788a0149d641069
ilvetrov <ilya-ilvetrov@gmail.com>
2021-07-05 11:47:32 +0800

    true-vh bug fix

--- source/js/partials/true-vh.js
+++ source/js/partials/true-vh.js
@@ -1,6 +1,7 @@
 let lastWindowWidth = window.innerWidth;
 window.addEventListener('resize', function() {
   if (lastWindowWidth === window.innerWidth) return;
+  lastWindowWidth = window.innerWidth;
 
   document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
 });
